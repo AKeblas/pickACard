7aea251b626d278aedb1c0744557060372b7e6b4
AKeblas <algirdaskeblas@yahoo.com>
2023-02-06 21:45:30 +0200

    update code

--- script.js
+++ script.js
@@ -1,54 +1,39 @@
 const main = document.querySelector(".main")
-const main2 = document.querySelector(".main2")
-const main3 = document.querySelector(".main3")
+const btn = document.querySelector("button")
 
 
-function getAll (){
-    fetch("http://167.99.138.67:1111/getallposts")
+let code = ""
+function getDeck () {
+    fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
         .then(res => res.json())
-        .then(posts => {
-            posts.data.map(user => {
-                main.innerHTML += `
-            <div class="box">
-                <h1 class="btn ${user.username}">${user.username}</h1>
-                <h2>${user.title}</h2>
-                <img class="${user.id} ${user.username}" src="${user.image}" alt="">
-                <p>${user.description}</p>
-                <h3>${user.id}</h3>
-                <p>${user.timestamp}</p>
-            </div>
-            `
-
-            })
-
-            const imgs = document.querySelectorAll("img")
-            let userName3 = ""
-            let userId3 = ""
-            imgs.forEach((el)=>{
-                el.onclick = (event) => {
-                    console.log(event)
-                    main.innerHTML = ""
-                    userName3 = event.target.classList[1]
-                    userId3 = event.target.classList[0]
-                    fetch("http://167.99.138.67:1111/getsinglepost/" + userName3 + "/" + userId3)
-                        .then(res => res.json())
-                        .then(user => {
-                            main.innerHTML = `
-                     <div class="box">
-                         <h1 class="btn ${user.data.username}">${user.data.username}</h1>
-                         <h2>${user.data.title}</h2>
-                         <img src="${user.data.image}" alt="">
-                         <p>${user.data.description}</p>
-                         <h3>${user.data.id}</h3>
-                         <p>${user.data.timestamp}</p>
-                     </div>
-                     `
-                        })
-                }
-            })
-
-
-
+        .then(data => {
+            code = data.deck_id
         })
 }
+getDeck()
+
+btn.onclick =()=>{
+    getPlayDeck(code)
+}
+
+
+function getPlayDeck (playId) {
+    fetch("https://www.deckofcardsapi.com/api/deck/"+ `${playId}` + "/draw/?count=1")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            main.innerHTML = `
+            <div class="d-flex">
+            <div class="box">
+                <h2>${data.remaining}</h2>
+            </div>
+            <div class="box">
+                <img src="${data.cards[0].image}" alt="">
+            </div>
+
+        </div>
+            `
+        })
+}
+
 
